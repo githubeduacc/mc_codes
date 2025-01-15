6a20ead894810def04979842e2dba9b650995dd4
Dante <denicolodante@gmail.com>
2025-01-15 05:19:24 -0300

    release

---
+++ README.md
@@ -0,0 +1,35 @@
+# Generador de códigos de descuento para Mc Donald's
+
+Generá códigos de 20%, 30%, 40%, 50%, 60% y 70% de descuento para utilizar desde la app de Mc Donald's de lunes a jueves hasta el 25 de enero del 2025. TyC: https://www.mcdonalds.com.ar/bases-y-condicones-promocion-ruletapp.
+
+Requisitos previos:
+
+- Instalar NodeJS: https://nodejs.org.
+
+## Instalación y configuración
+
+Se da por hecho que el código ya se encuentra descargado y que todos los comandos y acciones se realizarán dentro de la carpeta del mismo.
+
+1 - Instalar dependencias:
+
+```bash
+  npm install
+```
+
+2 - Renombrar archivo `.env.example` a `.env`.
+
+3 (opcional) - En caso de querer activar el servicio de notificaciones mediante email se deberá configurar el archivo `.env` con los campos `EMAIL_USER`, `EMAIL_PASSWORD`, `EMAIL_HOST`, `SENDER_ADDRESS`, y `RECEIVER_ADDRESS` según corresponda. Además se deberá cambiar el campo `EMAILS_ENABLED` de `false` a `true`.
+
+## Inicialización
+
+Para inicializar el programa ejecutá el siguiente comando:
+
+```bash
+  npm run start
+```
+
+En caso de querer inicializarlo en modo de desarrollo para depurar posibles errores, puedes hacerlo con este otro comando:
+
+```bash
+  npm run dev
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const fs = require("fs");
+const nodemailer = require("nodemailer");
+const axios = require("axios");
+
+const emailUser = process.env.EMAIL_USER;
+const emailPassword = process.env.EMAIL_PASSWORD;
+const emailHost = process.env.EMAIL_HOST;
+const emailPort = process.env.EMAIL_PORT;
+const senderAddress = process.env.SENDER_ADDRESS;
+const receiverAddress = process.env.RECEIVER_ADDRESS;
+const emailsEnabled = Boolean(process.env.EMAILS_ENABLED);
+
+const qtys = [70, 60, 50, 40, 30, 20];
+const logColors = {
+	red: "\x1b[31m",
+	green: "\x1b[32m",
+	yellow: "\x1b[33m",
+	blue: "\x1b[34m",
+	magenta: "\x1b[35m",
+	cyan: "\x1b[36m",
+};
+const qtyColors = {
+	70: logColors.magenta,
+	60: logColors.blue,
+	50: logColors.cyan,
+	40: logColors.yellow,
+	30: logColors.green,
+	20: logColors.green,
+};
+
+const app = express();
+
+const sendEmail = async ({ code, qty, isEnabled }) => {
+	let transporter = nodemailer.createTransport({
+		host: emailHost,
+		port: emailPort,
+		auth: {
+			user: emailUser,
+			pass: emailPassword,
+		},
+	});
+
+	const variables = {
+		success: {
+			subject: `¡Nuevo código de ${qty}% de descuento para McDonald's!`,
+			text: `Tu código para tu nuevo voto es: ${code}`,
+			html: `<h3>¡Se ha encontrado un nuevo código de ${qty}% de descuento para McDonald's!</h3>
+			</br>
+			<p>El código es: <strong>${code}</strong></p>
+            </br>
+            ${
+							isEnabled
+								? "<p><strong>El código está habilitado.</strong></p>"
+								: "<p><strong>El código no está habilitado.</strong></p>"
+						}
+            </br>               
+			<p>Email enviado automaticamente, no responder.</p>
+			`,
+		},
+	};
+
+	try {
+		await transporter.sendMail({
+			from: `Códigos Mc Donald's <${senderAddress}>`,
+			to: receiverAddress,
+			subject: variables["success"].subject,
+			text: variables["success"].text,
+			html: variables["success"].html,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const generateCode = (qty) => {
+	// format: QTYOFFXXXXX (QTY is two digit (20-30-40-50-60-70), X is a random character from A-Z or number from 0-9)
+	const code =
+		qty.toString() +
+		"OFF" +
+		Math.random().toString(36).substring(2, 7).toUpperCase();
+	return code;
+};
+
+const generateAndCheckCode = async (qty) => {
+	const code = generateCode(qty);
+
+	try {
+		const response = await axios.post(
+			"https://promos-mcd-ecommerce.appmcdonalds.com/api/promotions/check-code",
+			{
+				coupon: code,
+			},
+			{
+				headers: {
+					accept: "application/json, text/plain, */*",
+					"accept-language": "en-US,en;q=0.6",
+					"content-type": "application/json",
+					priority: "u=1, i",
+					"sec-ch-ua":
+						'"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+					"sec-ch-ua-mobile": "?0",
+					"sec-ch-ua-platform": '"Windows"',
+					"sec-fetch-dest": "empty",
+					"sec-fetch-mode": "cors",
+					"sec-fetch-site": "cross-site",
+					"sec-gpc": "1",
+					"x-app-country": "AR",
+					"x-app-version": "web-2.0.0",
+				},
+				referrerPolicy: "same-origin",
+			}
+		);
+		try {
+			const data = await response.data;
+			const isValid = data.valid;
+			const isEnabled = data.enabled;
+
+			if (isValid) {
+				// save the code to a file
+				fs.appendFileSync(
+					qty + "off.txt",
+					code + " ENABLED: " + isEnabled + "\n"
+				);
+
+				if (qty === 70 || qty === 60 || qty === 50 || qty === 40) {
+					console.log(
+						`${qtyColors[qty]} VALID ${qty}% OFF CODE: ${code} ENABLED: ${isEnabled}!!! ${qtyColors[qty]}`
+					);
+
+					emailsEnabled &&
+						sendEmail({
+							code: code,
+							qty: qty,
+							isEnabled: isEnabled,
+						});
+				} else {
+					console.log(
+						`${qtyColors[qty]} Valid ${qty}% off code: ${code} ENABLED: ${isEnabled} ${qtyColors[qty]}`
+					);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+setInterval(() => {
+	qtys.forEach((qty) => {
+		generateAndCheckCode(qty);
+	});
+}, 100);
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
