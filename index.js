4f0ae14f60f02fe7edfbf6d9bffc0d2a736dabfa
Dante <denicolodante@gmail.com>
2025-01-15 17:49:02 -0300

    Refactor email sending functionality and add code verification feature
    - Updated `sendEmail` function to handle multiple email types and include valid/invalid codes.
    - Introduced `checkExistingCodes` function to verify existing discount codes and log results.
    - Modified `.gitignore` to include `already_used.txt`.
    - Cleaned up code for better readability and maintainability.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const qtyColors = {
 
 const app = express();
 
-const sendEmail = async ({ code, qty, isEnabled }) => {
+const sendEmail = async ({ code, qty, type, validCodes, invalidCodes }) => {
 	let transporter = nodemailer.createTransport({
 		host: emailHost,
 		port: emailPort,
@@ -40,19 +40,41 @@ const sendEmail = async ({ code, qty, isEnabled }) => {
 	});
 
 	const variables = {
-		success: {
+		codeFound: {
 			subject: `¡Nuevo código de ${qty}% de descuento para McDonald's!`,
 			text: `Tu código para tu nuevo voto es: ${code}`,
 			html: `<h3>¡Se ha encontrado un nuevo código de ${qty}% de descuento para McDonald's!</h3>
 			</br>
 			<p>El código es: <strong>${code}</strong></p>
-            </br>
-            ${
-							isEnabled
-								? "<p><strong>El código está habilitado.</strong></p>"
-								: "<p><strong>El código no está habilitado.</strong></p>"
-						}
             </br>               
+			<p>Email enviado automaticamente, no responder.</p>
+			`,
+		},
+		checkedCodes: {
+			subject: `Resumen de verificación de códigos de ${qty}% descuento de McDonald's`,
+			text: `Resumen de verificación de códigos de ${qty}% descuento de McDonald's`,
+			html: `<h3>Resumen de verificación de códigos de ${qty}% descuento de McDonald's</h3>
+			</br>
+			<p>Se han verificado los códigos de ${qty}% descuento de McDonald's y se han encontrado los siguientes resultados:</p>
+			</br>
+			<h4>Códigos válidos (${validCodes.length}):</h4>
+			${
+				validCodes.length > 0
+					? "<ul>"
+					: "<p>No se han encontrado códigos válidos</p>"
+			}
+			${validCodes.map((code) => `<li>${code}</li>`).join("")}
+			${validCodes.length > 0 ? "</ul>" : ""}
+			</br>
+			<h4>Códigos inválidos ${invalidCodes.length}:</h4>
+			${
+				invalidCodes.length > 0
+					? "<ul>"
+					: "<p>No se han encontrado códigos inválidos</p>"
+			}
+			${invalidCodes.map((code) => `<li>${code}</li>`).join("")}
+			${invalidCodes.length > 0 ? "</ul>" : ""}
+			</br>
 			<p>Email enviado automaticamente, no responder.</p>
 			`,
 		},
@@ -62,9 +84,9 @@ const sendEmail = async ({ code, qty, isEnabled }) => {
 		await transporter.sendMail({
 			from: `Códigos Mc Donald's <${senderAddress}>`,
 			to: receiverAddress,
-			subject: variables["success"].subject,
-			text: variables["success"].text,
-			html: variables["success"].html,
+			subject: variables[type].subject,
+			text: variables[type].text,
+			html: variables[type].html,
 		});
 	} catch (error) {
 		console.log(error);
@@ -112,29 +134,26 @@ const generateAndCheckCode = async (qty) => {
 		try {
 			const data = await response.data;
 			const isValid = data.valid;
-			const isEnabled = data.enabled;
 
 			if (isValid) {
-				// save the code to a file
-				fs.appendFileSync(
-					qty + "off.txt",
-					code + " ENABLED: " + isEnabled + "\n"
-				);
+				fs.appendFileSync(qty + "off.txt", code + "\n");
 
 				if (qty === 70 || qty === 50) {
 					console.log(
-						`${qtyColors[qty]} VALID ${qty}% OFF CODE: ${code} ENABLED: ${isEnabled}!!! ${qtyColors[qty]}`
+						`${qtyColors[qty]} VALID ${qty}% OFF CODE: ${code}!!! ${qtyColors[qty]}`
 					);
 
 					emailsEnabled &&
-						sendEmail({
+						(await sendEmail({
 							code: code,
 							qty: qty,
-							isEnabled: isEnabled,
-						});
+							type: "codeFound",
+							validCodes: [],
+							invalidCodes: [],
+						}));
 				} else {
 					console.log(
-						`${qtyColors[qty]} Valid ${qty}% off code: ${code} ENABLED: ${isEnabled} ${qtyColors[qty]}`
+						`${qtyColors[qty]} Valid ${qty}% off code: ${code} ${qtyColors[qty]}`
 					);
 				}
 			}
@@ -145,6 +164,128 @@ const generateAndCheckCode = async (qty) => {
 		console.log(error);
 	}
 };
+
+const checkExistingCodes = async () => {
+	let validCodes = [];
+	let invalidCodes = [];
+	let totalCodesChecked = 0;
+
+	await Promise.all(
+		qtys.map(async (qty) => {
+			try {
+				const data = await fs.promises.readFile(qty + "off.txt", "utf8");
+				const codes = data.split(/\r?\n/).filter((code) => code.trim());
+				const invalidCodesForQty = [];
+				const validCodesForQty = [];
+
+				await Promise.all(
+					codes.map(async (code) => {
+						if (!code) return;
+						const codeValue = code.trim();
+
+						try {
+							const response = await axios.post(
+								"https://promos-mcd-ecommerce.appmcdonalds.com/api/promotions/check-code",
+								{ coupon: codeValue },
+								{
+									headers: {
+										accept: "application/json, text/plain, */*",
+										"accept-language": "en-US,en;q=0.6",
+										"content-type": "application/json",
+										priority: "u=1, i",
+										"sec-ch-ua":
+											'"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+										"sec-ch-ua-mobile": "?0",
+										"sec-ch-ua-platform": '"Windows"',
+										"sec-fetch-dest": "empty",
+										"sec-fetch-mode": "cors",
+										"sec-fetch-site": "cross-site",
+										"sec-gpc": "1",
+										"x-app-country": "AR",
+										"x-app-version": "web-2.0.0",
+									},
+									referrerPolicy: "same-origin",
+								}
+							);
+
+							const data = response.data;
+							const isValid = data.valid;
+							const status = data.status;
+
+							if (isValid) {
+								validCodesForQty.push(codeValue);
+								validCodes.push(codeValue);
+								if (qty === 70 || qty === 50) {
+									console.log(
+										`${qtyColors[qty]} ${qty}% OFF CODE: ${codeValue} IS STILL ENABLED!!! ${qtyColors[qty]}`
+									);
+								} else {
+									console.log(
+										`${qtyColors[qty]} ${qty}% off code: ${codeValue} is still enabled ${qtyColors[qty]}`
+									);
+								}
+							} else {
+								invalidCodesForQty.push(codeValue);
+								invalidCodes.push(codeValue);
+								console.log(
+									`${logColors.red} ${qty}% off code: ${codeValue} is not valid anymore. Status code: ${status} ${logColors.red}`
+								);
+							}
+							totalCodesChecked++;
+						} catch (error) {
+							console.log(error);
+						}
+					})
+				);
+
+				if (invalidCodesForQty.length > 0) {
+					await fs.promises.writeFile(
+						qty + "off.txt",
+						validCodesForQty.join("\n") + "\n"
+					);
+					await fs.promises.appendFile(
+						"already_used.txt",
+						invalidCodesForQty
+							.map((code) => `${qty}% OFF CODE: ${code}`)
+							.join("\n") + "\n"
+					);
+				}
+
+				await sendEmail({
+					code: "",
+					qty: qty,
+					type: "checkedCodes",
+					validCodes: validCodesForQty,
+					invalidCodes: invalidCodesForQty,
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		})
+	);
+
+	console.log("\n=== Resumen de verificación de códigos ===");
+	console.log(
+		`${logColors.yellow}Total de códigos verificados: ${totalCodesChecked}${logColors.yellow}`
+	);
+	console.log(
+		`${logColors.green}Códigos válidos: ${validCodes.length}${logColors.green}`
+	);
+	console.log(
+		`${logColors.red}Códigos inválidos: ${invalidCodes.length}${logColors.red}`
+	);
+	invalidCodes.length > 0 &&
+		console.log(
+			`${logColors.red}Códigos inválidos: ${invalidCodes.join(", ")}${
+				logColors.red
+			}`
+		);
+	console.log("=======================================\n");
+};
+
+(async () => {
+	await checkExistingCodes();
+})();
 
 setInterval(() => {
 	qtys.forEach((qty) => {
