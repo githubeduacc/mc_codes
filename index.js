3f52cc09bb607ae0e26bae6ed98b3acec7c7a518
Dante <denicolodante@gmail.com>
2025-01-15 23:31:14 -0300

    Refactor quantity settings and enhance code generation/checking logic
    - Updated qtys array to include detailed settings for each discount percentage, allowing for more granular control over code generation and checking.
    - Improved the logic for generating and checking codes to respect the new settings, including enabling/disabling notifications based on individual configurations.
    - Enhanced console output to display current settings for generating and checking codes, improving visibility for users.
    - Streamlined the process of reading and writing codes, ensuring that valid and invalid codes are handled appropriately based on the new structure.

--- index.js
+++ index.js
@@ -11,7 +11,38 @@ const senderAddress = process.env.SENDER_ADDRESS;
 const receiverAddress = process.env.RECEIVER_ADDRESS;
 const emailsEnabled = Boolean(process.env.EMAILS_ENABLED);
 
-const qtys = [70, 50, 30, 20];
+// Edit quantities settings here (percent: percentage off, gen_enabled: enable code generation, check_enabled: enable code checking, gen_notifications: enable generation notifications, check_notifications: enable checking notifications)
+const qtys = [
+	{
+		percent: 70,
+		gen_enabled: true,
+		check_enabled: true,
+		gen_notifications: true,
+		check_notifications: true,
+	},
+	{
+		percent: 50,
+		gen_enabled: true,
+		check_enabled: true,
+		gen_notifications: false,
+		check_notifications: true,
+	},
+	{
+		percent: 30,
+		gen_enabled: false,
+		check_enabled: false,
+		gen_notifications: false,
+		check_notifications: false,
+	},
+	{
+		percent: 20,
+		gen_enabled: false,
+		check_enabled: false,
+		gen_notifications: false,
+		check_notifications: false,
+	},
+];
+
 const logColors = {
 	red: "\x1b[31m",
 	green: "\x1b[32m",
@@ -139,19 +170,20 @@ const generateAndCheckCode = async (qty) => {
 			if (isValid) {
 				fs.appendFileSync(qty + "off.txt", code + "\n");
 
+				emailsEnabled &&
+					qtys.find((q) => q.percent === qty).gen_notifications &&
+					(await sendEmail({
+						code: code,
+						qty: qty,
+						type: "codeFound",
+						validCodes: [],
+						invalidCodes: [],
+					}));
+
 				if (qty === 70) {
 					console.log(
 						`${qtyColors[qty]} VALID ${qty}% OFF CODE: ${code}!!! ${qtyColors[qty]}`
 					);
-
-					emailsEnabled &&
-						(await sendEmail({
-							code: code,
-							qty: qty,
-							type: "codeFound",
-							validCodes: [],
-							invalidCodes: [],
-						}));
 				} else {
 					console.log(
 						`${qtyColors[qty]} Valid ${qty}% off code: ${code} ${qtyColors[qty]}`
@@ -173,96 +205,106 @@ const checkExistingCodes = async () => {
 	let invalidCodes = [];
 	let totalCodesChecked = 0;
 	console.log(
-		logColors.white + "\nVerificando de códigos existentes..." + logColors.white
+		logColors.white +
+			"\nVerificando de códigos existentes de " +
+			qtys
+				.filter((qty) => qty.check_enabled)
+				.map((qty) => qty.percent + "%")
+				.join(", ") +
+			"...\n" +
+			logColors.white
 	);
 
 	await Promise.all(
-		qtys.map(async (qty) => {
-			try {
-				const data = await fs.promises.readFile(qty + "off.txt", "utf8");
-				const codes = data.split(/\r?\n/).filter((code) => code.trim());
-				const invalidCodesForQty = [];
-				const validCodesForQty = [];
+		qtys
+			.filter((qty) => qty.check_enabled)
+			.map(async ({ percent: qty }) => {
+				try {
+					const data = await fs.promises.readFile(qty + "off.txt", "utf8");
+					const codes = data.split(/\r?\n/).filter((code) => code.trim());
+					const invalidCodesForQty = [];
+					const validCodesForQty = [];
 
-				await Promise.all(
-					codes.map(async (code) => {
-						if (!code) return;
-						const codeValue = code.trim();
+					await Promise.all(
+						codes.map(async (code) => {
+							if (!code) return;
+							const codeValue = code.trim();
 
-						try {
-							const response = await axios.post(
-								"https://promos-mcd-ecommerce.appmcdonalds.com/api/promotions/check-code",
-								{ coupon: codeValue },
-								{
-									headers: {
-										accept: "application/json, text/plain, */*",
-										"accept-language": "en-US,en;q=0.6",
-										"content-type": "application/json",
-										priority: "u=1, i",
-										"sec-ch-ua":
-											'"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-										"sec-ch-ua-mobile": "?0",
-										"sec-ch-ua-platform": '"Windows"',
-										"sec-fetch-dest": "empty",
-										"sec-fetch-mode": "cors",
-										"sec-fetch-site": "cross-site",
-										"sec-gpc": "1",
-										"x-app-country": "AR",
-										"x-app-version": "web-2.0.0",
-									},
-									referrerPolicy: "same-origin",
+							try {
+								const response = await axios.post(
+									"https://promos-mcd-ecommerce.appmcdonalds.com/api/promotions/check-code",
+									{ coupon: codeValue },
+									{
+										headers: {
+											accept: "application/json, text/plain, */*",
+											"accept-language": "en-US,en;q=0.6",
+											"content-type": "application/json",
+											priority: "u=1, i",
+											"sec-ch-ua":
+												'"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+											"sec-ch-ua-mobile": "?0",
+											"sec-ch-ua-platform": '"Windows"',
+											"sec-fetch-dest": "empty",
+											"sec-fetch-mode": "cors",
+											"sec-fetch-site": "cross-site",
+											"sec-gpc": "1",
+											"x-app-country": "AR",
+											"x-app-version": "web-2.0.0",
+										},
+										referrerPolicy: "same-origin",
+									}
+								);
+
+								const data = response.data;
+								const isValid = data.valid;
+								const status = data.status;
+
+								if (isValid) {
+									validCodesForQty.push(codeValue);
+									validCodes.push(codeValue);
+								} else {
+									invalidCodesForQty.push(codeValue);
+									invalidCodes.push(codeValue);
+									console.log(
+										`${logColors.red} ${qty}% off code: ${codeValue} is not valid anymore. Status code: ${status} ${logColors.red}`
+									);
 								}
-							);
-
-							const data = response.data;
-							const isValid = data.valid;
-							const status = data.status;
-
-							if (isValid) {
-								validCodesForQty.push(codeValue);
-								validCodes.push(codeValue);
-							} else {
-								invalidCodesForQty.push(codeValue);
-								invalidCodes.push(codeValue);
+								totalCodesChecked++;
+							} catch (error) {
 								console.log(
-									`${logColors.red} ${qty}% off code: ${codeValue} is not valid anymore. Status code: ${status} ${logColors.red}`
+									`${logColors.yellow} Error checking ${qty}% off code: ${codeValue} - ${error.code} ${logColors.yellow}`
 								);
 							}
-							totalCodesChecked++;
-						} catch (error) {
-							console.log(
-								`${logColors.yellow} Error checking ${qty}% off code: ${codeValue} - ${error.code} ${logColors.yellow}`
-							);
-						}
-					})
-				);
+						})
+					);
 
-				if (invalidCodesForQty.length > 0) {
-					await fs.promises.writeFile(
-						qty + "off.txt",
-						validCodesForQty.join("\n") + "\n"
-					);
-					await fs.promises.appendFile(
-						"already_used.txt",
-						invalidCodesForQty
-							.map((code) => `${qty}% OFF CODE: ${code}`)
-							.join("\n") + "\n"
-					);
+					if (invalidCodesForQty.length > 0) {
+						await fs.promises.writeFile(
+							qty + "off.txt",
+							validCodesForQty.join("\n") + "\n"
+						);
+						await fs.promises.appendFile(
+							"already_used.txt",
+							invalidCodesForQty
+								.map((code) => `${qty}% OFF CODE: ${code}`)
+								.join("\n") + "\n"
+						);
+					}
+
+					emailsEnabled &&
+						qtys.find((q) => q.percent === qty).check_notifications &&
+						invalidCodesForQty.length > 0 &&
+						(await sendEmail({
+							code: "",
+							qty: qty,
+							type: "checkedCodes",
+							validCodes: validCodesForQty,
+							invalidCodes: invalidCodesForQty,
+						}));
+				} catch (error) {
+					console.log(logColors.red + error + logColors.red);
 				}
-
-				emailsEnabled &&
-					invalidCodesForQty.length > 0 &&
-					(await sendEmail({
-						code: "",
-						qty: qty,
-						type: "checkedCodes",
-						validCodes: validCodesForQty,
-						invalidCodes: invalidCodesForQty,
-					}));
-			} catch (error) {
-				console.log(logColors.red + error + logColors.red);
-			}
-		})
+			})
 	);
 
 	console.log(
@@ -305,13 +347,39 @@ setInterval(() => {
 
 setInterval(() => {
 	if (checkingCodes) return;
-	qtys.forEach((qty) => {
-		generateAndCheckCode(qty);
-	});
+	qtys
+		.filter((qty) => qty.gen_enabled)
+		.forEach((qty) => {
+			generateAndCheckCode(qty.percent);
+		});
 }, 100);
 
 app.listen(3000, () => {
 	console.log(
 		logColors.white + "Server is running on port 3000" + logColors.white
+	);
+	console.log(
+		logColors.blue +
+			"CURRENT SETTINGS: \nGENERATING CODES FOR: " +
+			qtys
+				.filter((qty) => qty.gen_enabled)
+				.map((qty) => qty.percent + "%")
+				.join(", ") +
+			"\nCHECKING CODES FOR: " +
+			qtys
+				.filter((qty) => qty.check_enabled)
+				.map((qty) => qty.percent + "%")
+				.join(", ") +
+			"\nGENERATION NOTIFICATIONS: " +
+			qtys
+				.filter((qty) => qty.gen_notifications)
+				.map((qty) => qty.percent + "%")
+				.join(", ") +
+			"\nCHECK NOTIFICATIONS: " +
+			qtys
+				.filter((qty) => qty.check_notifications)
+				.map((qty) => qty.percent + "%")
+				.join(", ") +
+			logColors.blue
 	);
 });
